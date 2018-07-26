10f7952b6a751b5ea2d5501f2854d439491fef67
zhengxu1 <zhengxu1@huya.com>
2018-07-26 16:45:53 +0800

    细化返回类型

---
+++ communication/src/main/java/com/coordinate/entity/CodeType.java
@@ -0,0 +1,9 @@
+package com.coordinate.entity;
+
+public class CodeType {
+
+    public static final int SUCCESS = 0;
+    public static final int UNKNOWN_ERROR = -1;
+    public static final int UNMATCHED_BROKER = 1001;
+
+}
--- communication/src/main/java/com/coordinate/entity/ResponseType.java
+++ communication/src/main/java/com/coordinate/entity/ResponseType.java
@@ -1,8 +1,10 @@
 package com.coordinate.entity;
 
+import org.omg.CORBA.UNKNOWN;
+
 public class ResponseType {
 
-    public static final Response<Object> SIMPLE_SUCCESS = new Response<Object>(0,"success",null);
-    public static final Response<Object> UNKNOWN_FAIL = new Response<Object>(-1,"unknown",null);
+    public static final Response<Object> SIMPLE_SUCCESS = new Response<Object>(CodeType.SUCCESS,"success",null);
+    public static final Response<Object> UNKNOWN_FAIL = new Response<Object>(CodeType.UNKNOWN_ERROR,"unknown",null);
 
 }
